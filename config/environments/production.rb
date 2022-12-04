70852313a54dcba6370980b90db10e35b59dec56
chikiuto <abetokichiro@gmail.com>
2022-12-05 00:04:07 +0900

    manifest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.assets.enabled = false
-
   # Settings specified here will take precedence over those in config/application.rb.
   # Code is not reloaded between requests.
   config.cache_classes = true
