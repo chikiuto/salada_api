f9421285434eb57ae1eea191993f518fdaa2e56f
chikiuto <abetokichiro@gmail.com>
2022-12-05 17:12:31 +0900

    error handling

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/' => 'top#index'
   get 'recipes/index' => 'recipes#index'
   post 'report/create' => 'reports#create'
+
+  get '*path', controller: 'application', action: 'render_404'
 end
