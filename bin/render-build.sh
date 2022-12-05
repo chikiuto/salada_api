c0fc4d9dbc90a167c35f48322eb95151af793088
chikiuto <abetokichiro@gmail.com>
2022-12-05 18:56:08 +0900

    see document

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,13 +3,6 @@
 set -o errexit
 
 bundle install
-yarn install
-
-# jsファイルをesbuildでバンドルしているため
-yarn build 
-
-# cssはsprocketsを使っているため
-bundle exec rake assets:precompile 
-
-# migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
-bundle exec ridgepole -c config/database.yml -E production --apply -f db/schemas/Schemafile 
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
