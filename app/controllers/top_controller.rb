99e473c5d8383e8973ce203073cba6b5d27461c0
chikiuto <abetokichiro@gmail.com>
2022-12-11 18:27:02 +0900

    left outer join

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,11 +1,13 @@
 class TopController < ApplicationController
 	def index
 		# @reports = Report.all.order(created_at: :desc)
-		@report = Report.all.joins(
-			"LEFT OUTER JOIN recipes ON reports.recipe_id = recipes.id")
-		.select("recipes.*")
-		.order(created_at: :desc)
-		# .each do |results| puts "#{results.title}: #{results.id}" end
+
+		@reports = Report.joins(
+			"LEFT OUTER JOIN recipes 
+				ON reports.recipe_id = recipes.id"
+			)
+			.select("reports.*", "recipes.*")
+			.order(created_at: :desc)
 		render json: @reports
 	end
 end
