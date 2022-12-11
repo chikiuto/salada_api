0b8e12ea7f83a35ab4729dba1e22eb54b35768f6
chikiuto <abetokichiro@gmail.com>
2022-12-11 17:24:23 +0900

    add sql

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,11 @@
 class TopController < ApplicationController
 	def index
-		@reports = Report.all.order(created_at: :desc)
+		# @reports = Report.all.order(created_at: :desc)
+		@report = Report.all.joins(
+			"LEFT OUTER JOIN recipes ON reports.recipe_id = recipes.id")
+		.select("recipes.*")
+		.order(created_at: :desc)
+		# .each do |results| puts "#{results.title}: #{results.id}" end
 		render json: @reports
 	end
 end
